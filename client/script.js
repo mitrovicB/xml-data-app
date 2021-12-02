e60d7e72ad30f83e354a10546cbc08b374685979
Branka Mitrovic <bramit93.gmail.com>
2021-12-02 19:12:22 +0100

    user import from xml solved

--- client/script.js
+++ client/script.js
@@ -3,3 +3,25 @@ registerBtn.addEventListener('click', () => {
     console.log('save user');
     }
 );
+
+function loadXMLDoc(callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    console.log(this.readyState,this.status);
+      if (this.readyState == 4 && this.status == 200) {
+          console.log(this.responseText)
+        callback(this);
+      }
+    };
+    xhttp.open("GET", "http://127.0.0.1:3000/users", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send();
+}
+
+function getUsers(xml) {
+    let i, txt, y, xmlDoc, data;
+    xmlDoc = xml.response;
+    console.log(xmlDoc);
+}
+
+loadXMLDoc(getUsers);
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,6 @@
-//parser = require('xml2json'),
+//const parser = require('xml2json'),
     const fs = require('fs');
-        xml2js = require('xml2js');
+    parseString = require('xml2js').parseString;
 
 
 module.exports = app => {
@@ -11,17 +11,32 @@ module.exports = app => {
   
     // Get all users
     app.get('/users', (req, res) => {
-        let users;
-        let parser = new xml2js.Parser();
-        fs.readFile('./data.xml', function(err, data) {
-            parser.parseString(data, function (err, result) {
-            console.dir(result['data']['continent'][0])
-            /** U ovom dijelu sam zaglavila, 
-             * trebam izvuci sve korisnike  */
-                continents = result['data']['continent'];
-                users = continents[0];
+        let allUsers = [];
+        fs.readFile('./data.xml', 'utf8', function(err, data) {
+            parseString(data, function (err, result) {
+            const json = JSON.stringify(result, null, 4);
+            const el = JSON.parse(json);
+            const continents = el.data.continent;
+            console.log(continents);
+            continents.forEach(continent => {
+                console.log(continent.$.name);
+                let countries = continent.country;
+
+                countries.forEach(country => {
+                    console.log(country.$.name)
+                    users = country.user;
+                    allUsers.push(users);
+                    users.forEach(user => {
+                        console.log('First name ' + user.first_name + ' Last name ' + user.last_name +
+                            ' Address ' + user.address + ' City ' + user.city + ' Email ' + user.email +
+                            ' Password ' + user.password);
+                        console.log('\n');
+                    });
+                });
             });
-            res.send(users);
+            console.log(allUsers)
+            res.send(allUsers);
+            });
         });
     });
 
@@ -30,3 +45,32 @@ module.exports = app => {
         console.log('delete function');
     })
 }
+
+/**
+    parseString(data, function (err, result) {
+        const json = JSON.stringify(result, null, 4);
+        const el = JSON.parse(json);
+        const continents = el.data.continent;
+
+        continents.forEach(continent => {
+            console.log(continent.$.name);
+            let countryes = continent.country;
+
+            countryes.forEach(country => {
+                console.log(country.$.name);
+                let users = country.user;
+
+                users.forEach(user => {
+                    console.log('First name ' + user.first_name + ' Last name ' + user.last_name +
+                        ' Address ' + user.address + ' City ' + user.city + ' Email ' + user.email +
+                        ' Password ' + user.password);
+                    console.log('\n');
+                })
+            })
+        });
+    });
+});
+Write to Bojan Todorovic
+Aa
+
+ */
