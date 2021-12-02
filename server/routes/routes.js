fd8a021aa4a69d05bf015ba2ec8338347a9b4d99
Branka Mitrovic <bramit93.gmail.com>
2021-12-02 18:15:02 +0100

    Unuseful function removed

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,15 +4,6 @@
 
 
 module.exports = app => {
-   /* function getXml() {
-        fs.readFile('./data.xml', function(err, data) {
-        if (err) console.log(err);
-        const json = JSON.parse(parser.toJson(data, {reversible: true}));
-        console.log("json:", json.data);
-        return json;
-        });
-    }   */
-
     // Create a User
     app.post("/register", (req, res) => {
         console.log('signup function')
