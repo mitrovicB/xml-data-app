804f43011261cd79057debf9eac622284fae49bd
Branka Mitrovic <bramit93.gmail.com>
2021-12-04 14:10:33 +0100

    Delete function update

--- client/script.js
+++ client/script.js
@@ -3,13 +3,15 @@ registerBtn.addEventListener('click', () => {
   saveUserInfo();
   }
 );
-saveUserInfo();
 
-// loadXMLDoc(getUsers, "GET", "users");
+loadXMLDoc(getUsers, "GET", "users");
 
-function deleteUser() {
-  console.log('delete user');
+function deleteUser(user) {
+  let userId = user.parentNode.id
+  console.log('delete user:', userId);
+  console.log(JSON.stringify(userId));
 }
+
 function showError(msg) {
   //let errorDiv = document.getElementById('error');
   console.log(msg);
@@ -25,9 +27,8 @@ function saveUserInfo() {
     email: document.querySelector('[name="email"]').value,
     password: document.querySelector('[name="password"]').value
   };
-
   console.log(user);
-
+  
   // test email format https://ui.dev/validate-email-address-javascript/
   if (! /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.email)) {
     showError("Invalid email format!");
@@ -35,54 +36,62 @@ function saveUserInfo() {
   }
   if (user.password.length < 8) {
       showError("Password must be at least 8 characters long!");
-      return;
+      return;book
   }
 
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          console.log(xhttp.responseText);
-      } else if (this.status == 400) {
-        return showError('You must fill all fields!');
-      }
+    switch(this.status) {
+      case 200:
+        console.log(xhttp.responseText);
+        break;
+      case 400:
+        showError('You must fill all fields!');
+        break;
+      case 409:
+        showError('This email address is already in use!');
+      break;
+      default:
+        showError('Some error occured.')
+    }
   };
-
   xhttp.open("POST", "http://127.0.0.1:3000/register", true);
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(JSON.stringify(user));
 }
 
-function loadXMLDoc(callback, method, route, newObj) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+function loadXMLDoc(callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
     console.log(this.readyState,this.status);
       if (this.readyState == 4 && this.status == 200) {
         console.log(this.responseText);
         callback(this);
       }
-    };
-    xhttp.open(method, "http://127.0.0.1:3000/" + route, true);
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(newObj);
+  };
+  xhttp.open("GET", "http://127.0.0.1:3000/users", true);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send();
 }
 
 function getUsers(xml) {
-    let i, data;
-    data = JSON.parse(xml.responseText);
-    console.log(data);
-    data.forEach(user => {
-    let table = document.querySelector("table");
-    table.innerHTML += `
-    <tr>
+  let i, data;
+  data = JSON.parse(xml.responseText);
+  console.log(data);
+  let numberId = 1;
+  data.forEach(user => {
+  let table = document.querySelector("tbody");
+  let userId = 'user' + numberId++;
+  table.innerHTML += `
+    <tr id=${userId}>
       <td>${user.first_name[0]}</td>
       <td>${user.address[0]}</td>
       <td>${user.city[0]}</td>
       <td>${user.country}</td>
       <td>${user.email[0]}</td>
-      <td><button class="delete-btn" onclick="deleteUser()">delete</button></td>
+      <td><button type="submit" id="delete-btn" onclick='deleteUser(this.parentNode)'>delete</button></td>
     </tr>
     `
-    });
-  }
-
-
+  console.log(userId);
+  });
+}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,22 +2,15 @@ const fs = require('fs'),
     xml2js = require('xml2js'),
     parseString = xml2js.parseString;
 
-const continentArr = {
+const continentObj = {
     "Europe": ["United Kingdom", "Germany", "France"],
     "North America": ["United States", "Canada"],
     "East Asia": ["China", "Japan"]
-}
-
-const createAttr = (attr) => {
-    console.log('attr', attr);
-    let newAtrr = 'country.$.name';
-    return newAtrr;
-}
+};
 
 module.exports = app => {
     // Create a User
     app.post("/register", (req, res) => {
-
         // Create User
         const newUser = {
             first_name: req.body.first_name,
@@ -30,19 +23,7 @@ module.exports = app => {
         };
         console.log(newUser);
 
-        for (let key in continentArr) {
-            let obj = continentArr[key];
-            console.log(obj)
-            obj.forEach(country => {
-                console.log(country);
-                if (newUser.country === country) {
-                    createAttr(country);
-                }
-            // need to stop forEach when condition is met
-            });
-        }
-
-         /* for (let [key, value] of Object. entries(user)) {
+          /* for (let [key, value] of Object. entries(user)) {
                 if(value === '') {
                     res.status(400).send({
                         message: "Content can not be empty!"
@@ -51,25 +32,59 @@ module.exports = app => {
                 }
             } */
 
+        let userContinent;
+        for(continent in continentObj) {
+            if(continentObj.hasOwnProperty(continent)) {
+                let countryArr = continentObj[continent];
+                console.log("continent: ", continent + "country array: ", countryArr)
+                countryArr.forEach(el => {
+                    if (newUser.country === el) {
+                        userContinent = continent;
+                        console.log("user continent:", userContinent);
+                    }
+                });
+            }
+        }
         fs.readFile('./data.xml', 'utf8', function(err, data) {
             parseString(data, function (err, result) {
             const json = JSON.stringify(result, null, 4);
             const el = JSON.parse(json);
             const continents = el.data.continent;
             console.log(continents);
+
                 continents.forEach(continent => {
-                    console.log(continent.$.name);
+                    console.log("continent name:", continent.$.name);
                     let countries = continent.country;
                     countries.forEach(country => {
-                        console.log("country name: "+ country.$.name);
+                        console.log("country name: " + country.$.name);
                         users = country.user;
-                        console.log("users: ", users);
-                          
-                    })
-                })
-            })
-        })
-    })
+                        users.forEach(user => {
+                            console.log(user.email);
+                            console.log(newUser.email)
+                            if (newUser.email == user.email) {
+                                return res.status(409).json({
+                                    message:
+                                      err || "This email address is already in use."
+                                  })
+                                } else {
+                                if (newUser.country === country.$.name) {
+                                    console.log("new user goes here:", country.$.name);
+                                    country.user.push(newUser);
+                                    console.log(country);
+                                } else {
+                                    console.log("make new continent");
+                                    console.log(continent.$.name);
+                                    return;
+                                }
+                            }
+                        })
+                    });
+                });
+            });
+        });
+      // write File
+    });
+
     // Get all users
     app.get('/users', (req, res) => {
         let allUsers = [];
@@ -88,7 +103,7 @@ module.exports = app => {
                     users = country.user;
 
                     users.forEach(user => {
-                        user.country = country.$.name;
+                                               user.country = country.$.name;
                         console.log('First name ' + user.first_name + ' Last name ' + user.last_name +
                             ' Address ' + user.address + ' City ' + user.city + 'Country ' + user.country + ' Email ' + user.email +
                             ' Password ' + user.password);
